9e78de2958d55697d3814bc2c94e7902e04d257b
jaesharma <44082416+jaesharma@users.noreply.github.com>
2020-06-26 15:17:10 +0530

    users's data is private now

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -7,7 +7,8 @@ export const addExpense=(expense)=>({
 });
 
 export const startAddExpense=(expenseData={})=>{
-	return (dispatch)=>{
+	return (dispatch,getState)=>{
+		const uid=getState().auth.uid;
 		const {
 			description='',
 			note='',
@@ -16,7 +17,7 @@ export const startAddExpense=(expenseData={})=>{
 		}=expenseData;
 		const expense={ description, note, amount, createdAt};
 
-		database.ref('expenses').push(expense).then((ref)=>{
+		database.ref(`users/${uid}/expenses`).push(expense).then((ref)=>{
 			dispatch(addExpense({
 				id: ref.key,
 				...expense
@@ -31,8 +32,9 @@ export const removeExpense=({id}={})=>({
 });
 
 export const startRemoveExpense=({id}={})=>{
-	return (dispatch)=>{
-		database.ref(`expenses/${id}`).remove().then((ref)=>{
+	return (dispatch,getState)=>{
+		const uid=getState().auth.uid;
+		database.ref(`users/${uid}/expenses/${id}`).remove().then((ref)=>{
 			dispatch(removeExpense({
 				id
 			}))
@@ -47,8 +49,9 @@ export const editExpense=(id,updates)=>({
 });
 
 export const startEditExpense=(id,updates)=>{
-	return (dispatch)=>{
-		return database.ref(`expenses/${id}`).update(updates).then(()=>{
+	return (dispatch,getState)=>{
+		const uid=getState().auth.uid;
+		return database.ref(`users/${uid}/expenses/${id}`).update(updates).then(()=>{
 			dispatch(editExpense(id,updates));
 		})
 	}
@@ -60,8 +63,9 @@ export const setExpenses=(expenses)=>({
 });
 
 export const startSetExpenses=()=>{
-	return (dispatch)=>{
-		return database.ref('expenses').once('value').then((snapshot)=>{
+	return (dispatch,getState)=>{
+		const uid=getState().auth.uid;
+		return database.ref(`users/${uid}/expenses`).once('value').then((snapshot)=>{
 			const expenses=[];
 			snapshot.forEach((expense)=>{
 				expenses.push({
--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -8,17 +8,18 @@ import EditExpensePage from '../components/EditExpensePage';
 import HelpPage from '../components/HelpPage';
 import NotFound from '../components/NotFound';
 import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
 
 export const history=createBrowserHistory();
 
 const AppRouter=()=>(
 	<Router history={history}>
 		  <Switch>
-		  	<Route path="/" component={LoginPage} exact={true} />
+		  	<PublicRoute path="/" component={LoginPage} exact={true} />
 		  	<PrivateRoute path="/dashboard" component={ExpenseDashboardPage} exact={true} />
 		  	<PrivateRoute path="/create" component={AddExpensePage} />
 		  	<PrivateRoute path="/edit/:id" component={EditExpensePage} />
-		  	<Route component={NotFound} />
+		  	<PublicRoute component={NotFound} />
 		  </Switch>
 	</Router>
 );
